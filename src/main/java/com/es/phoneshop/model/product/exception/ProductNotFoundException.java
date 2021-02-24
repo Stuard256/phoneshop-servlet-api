e7c351a05be01662bc91eaae5339aa4087791de0
Stuard256 <43059880+Stuard256@users.noreply.github.com>
2021-02-24 15:24:22 +0300

    fixed several mistakes

--- src/main/java/com/es/phoneshop/model/product/exception/ProductNotFoundException.java
+++ src/main/java/com/es/phoneshop/model/product/exception/ProductNotFoundException.java
@@ -1,5 +1,5 @@
 package com.es.phoneshop.model.product.exception;
 
-public class ProductNotFoundException extends RuntimeException{
+public class ProductNotFoundException extends RuntimeException {
 
 }
