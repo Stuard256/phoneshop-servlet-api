7fe00f704195bac75ffd8d138b0c89820e422cf3
Stuard256 <43059880+Stuard256@users.noreply.github.com>
2021-03-07 13:12:49 +0300

    Several bugs fixed

--- src/test/java/com/es/phoneshop/entity/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/entity/ArrayListProductDaoTest.java
@@ -1,4 +1,4 @@
-package com.es.phoneshop.model.product;
+package com.es.phoneshop.entity;
 
 import com.es.phoneshop.DAO.Product.ArrayListProductDao;
 import com.es.phoneshop.DAO.Product.ProductDao;
